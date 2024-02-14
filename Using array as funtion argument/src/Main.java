b79f877c641d5ce6e8ba25cf1f1dc272733f6ef3
Santhosh1043k <159894976+Santhosh1043k@users.noreply.github.com>
2024-02-14 13:43:11 +0530

    Add files via upload

---
+++ Using array as funtion argument/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int a[]= {1, 2, 3, 4, 5, 6, 7, 8};
+
+        printArray(a);
+    }
+    public static void printArray(int x[]) {
+        for (int y:x) {
+            System.out.println(y);
+        }
+
+    }
+
+    }
